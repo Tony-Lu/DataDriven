e02e8f6bdc0d13a2372d50c89af36705fbe2ea50
Tony Lu <xudong.lu@yahoo.com>
2019-04-17 03:38:07 +0800

    this is from Eclipse

--- src/test/java/com/w2a/rough/TestTimeStamp.java
+++ src/test/java/com/w2a/rough/TestTimeStamp.java
@@ -7,6 +7,7 @@ public class TestTimeStamp {
 	public static void main(String[] args) {
 		
 		//this is for test jenkins trigger new build once git checked in
+		//using Eclipse plugin commit to git
 		Date d = new Date();
 		String screenShotName = d.toString().replace(":", "_").replace(" ",	"_")+".jpg";
 		
